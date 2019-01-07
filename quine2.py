c315ba415bf4f8a3ee4763fb0dbd518d75ab10d6
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-01-07 04:22:20 -0800

    first commit

---
+++ quine2.py
@@ -0,0 +1,28 @@
+def quine():
+	tab = 9
+	quote = 34
+	comma = 44
+	l = [
+		"def quine():",
+		"	tab = 9",
+		"	quote = 34",
+		"	comma = 44",
+		"	l = [",
+		"	]",
+		"for i in range(0, 5):",
+		"	print(l[i])",
+		"for element in l:",
+		"	 print(chr(tab) + chr(tab) + chr(quote) + element + chr(quote) + chr(comma))",
+		"for i in range(5, len(l)):",
+		"	print(chr(tab) + chr(tab) + l[i])",
+		"print(l[-1])",
+		"quine()",
+		]
+	for i in range(0, 5):
+		print(l[i])
+	for element in l:
+		 print(chr(tab) + chr(tab) + chr(quote) + element + chr(quote) + chr(comma))
+	for i in range(5, len(l)):
+		print(chr(tab) + chr(tab) + l[i])
+	print(l[-1])
+quine()
